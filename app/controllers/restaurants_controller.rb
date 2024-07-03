5b250713f8132d36a3f161f0e1e6c084f86b1f20
Rahel Pauli <rahel.pauli@gmx.de>
2024-07-03 16:34:13 +0200

    implemented list restaurants and add restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_created_path(@restaurant)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
